cd2272067156799a7d661a471d21bf5640673cd0
MarcoBlzz <U201922326@upc.edu.pe>
2025-06-23 20:03:01 -0500

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { TipoEnfermedadComponent } from './components/tipoenfermedad/tipoenfermedad.component';
+import { InsertareditarComponent } from './components/tipoenfermedad/insertareditar/insertareditar.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  {
+    path:'',redirectTo:'tipoenfermedades/listas',pathMatch:'full'
+  },
+  {
+    path:'tipoenfermedades/listas',component:TipoEnfermedadComponent,
+    children:[
+      {
+        path:'formulario',component:InsertareditarComponent
+      },
+      {
+        path:'ediciones/:id',component:InsertareditarComponent
+      },
+   
+    ]
+  }
+];
